149e3f26a80ad08cdf846b5365c7d2d69a65b915
NeedforGit <48271689+NeedforGit@users.noreply.github.com>
2020-12-30 11:17:05 +0800

    Update diy.sh

--- devices/x86_64/diy.sh
+++ devices/x86_64/diy.sh
@@ -4,3 +4,5 @@ echo '
 CONFIG_CRYPTO_CHACHA20_X86_64=y
 CONFIG_CRYPTO_POLY1305_X86_64=y
 ' >> ./target/linux/x86/64/config-5.4
+
+sed -i 's/10.0.0.1/192.168.1.1/g' package/base-files/files/bin/config_generate
